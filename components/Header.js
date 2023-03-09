d8dd4cd64e1d1f6abfa27c2c5f0582f48eb0a109
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-03-09 14:08:44 +0530

    made login page

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+        <h1 className='text-3xl select-none sm:text-6xl'>Keeper</h1>
+        <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+
+    </div>
+  )
+}
+
+export default Header
