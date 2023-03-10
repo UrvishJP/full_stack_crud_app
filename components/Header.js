6737e86a4c5b3f598124f51d6c85f1c64a1f1a0f
Urvish Prajapati <urvishmeghani111@gmail.com>
2023-03-10 15:01:51 +0530

    finishing up

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Modal from './Modal'
 
-const Header = () => {
-  return (
-    <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
-        <h1 className='text-3xl select-none sm:text-6xl'>Keeper</h1>
-        <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+export default function Header() {
+    const [openModal, setOpenModal] = useState(false)
 
-    </div>
-  )
+    return (
+        <>
+            {openModal && <Modal setOpenModal={setOpenModal} />}
+            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+                <h1 className='text-3xl select-none sm:text-6xl'>QuickNote</h1>
+                <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+            </div>
+        </>
+    )
 }
-
-export default Header
