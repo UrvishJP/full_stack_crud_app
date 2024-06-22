efee5516657cfa213621590c1a6260338c589619
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-06-22 10:10:38 +0530

    header bug fixed

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
     return (
         <>
             {openModal && <Modal setOpenModal={setOpenModal} />}
-            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white z-50'>
                 <h1 className='text-3xl select-none sm:text-6xl'>QuickNote</h1>
                 <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
             </div>
